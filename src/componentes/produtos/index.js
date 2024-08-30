b67872070b4354f7c3699e6fad05f583130f13f7
Enzo-dorta <91228736+Enzo-dorta@users.noreply.github.com>
2024-08-30 18:35:40 +0000

    salvando o processo

--- src/componentes/produtos/index.js
+++ src/componentes/produtos/index.js
@@ -1,11 +1,12 @@
+import {} from '../../index.css';
 
 const Produto = ({valor}) =>{
     return(
         <div>
-            <h3>
+            <h3 className='titulo'>
                 {valor.nome}
             </h3>
-            <p>
+            <p className='subtitulo'>
                 Preço: {valor.preco}
             </p>
         </div>
